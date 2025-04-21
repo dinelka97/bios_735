ac910d0db7e26ab31f395d252ac4cc88ccc38a19
Matthew Sutcliffe <mdsutcliffe5@gmail.com>
2025-04-21 09:44:00 -0400

    clean up to just include RF results before analysis

--- code/randomforest_run.R
+++ code/randomforest_run.R
@@ -18,37 +18,10 @@ x_test <- x_test[, -which(names(x_test) == "num")]
 meta <- read.csv(file = "./data/derived/var_info_upd.csv")
 
 
-
-
 # Fit ordinal model (takes a while)
 res_multi <- fit_rf(x, y_multi)
-
-df_multi_train <- cbind(x, y_multi) |> as.data.frame()
-names(df_multi_train)[ncol(df_multi_train)] <- "Class"
-pred_multi_train <- predict(object = res_multi$fit, newdata = df_multi_train)
-table("prediction" = pred_multi_train$ypred, "true label" = df_multi_train$Class)
-
-
-df_multi_test <- cbind(x_test, y_multi_test) |> as.data.frame()
-names(df_multi_test)[ncol(df_multi_test)] <- "Class"
-pred_multi_test <- predict(object = res_multi$fit, newdata = df_multi_test)
-table("prediction" = pred_multi_test$ypred, "true label" = df_multi_test$Class)
 
 # Fit ordinal model but instead let's classify into the binary output (0, > 0)
 res_binary <- fit_rf(x, y_binary)
 
-
-df_binary_train <- cbind(x, y_binary) |> as.data.frame()
-names(df_binary_train)[ncol(df_binary_train)] <- "Class"
-pred_binary_train <- predict(object = res_binary$fit, newdata = df_binary_train)
-table("prediction" = pred_binary_train$ypred, "true label" = df_binary_train$Class)
-
-
-df_binary_test <- cbind(x_test, y_binary_test) |> as.data.frame()
-names(df_binary_test)[ncol(df_binary_test)] <- "Class"
-pred_binary_test <- predict(object = res_binary$fit, newdata = df_binary_test)
-table("prediction" = pred_binary_test$ypred, "true label" = df_binary_test$Class)
-
-
 save.image(file = "./results/randomforest_results.RData")
-# Seeing that it performs much better on the binary output than the multi-class.
