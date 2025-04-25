80d8b1a9e52f5f78ce5e18e70e85d3d5f0a6d51a
Dinelka Nanayakkara <dink.nana97@gmail.com>
2025-04-25 18:05:18 -0400

    Uploading optimization and estimation scripts

---
+++ code/estimation.R
@@ -0,0 +1,196 @@
+# Introduction ------------------------------------------------------------
+
+  ## -- this script serves the purpose of defining the functions that are to be included within our package so that a user can -
+    ## -- use it to run the proportional odds logistic regression model.
+  ## -- a separate script is used to run the same for logistic regression.
+
+# Load libraries ----------------------------------------------------------
+
+library(tidyverse)
+library(magrittr)
+library(glue)
+library(ggplot2)
+library(MASS)
+
+### -- example of running a proportional odds model
+
+  ## -- the user can provide a data frame (subset as required) and obtain estimates.
+  ## -- user has to specify which variable is the response variable (everything else will be covariates).
+  ## -- this function assumes that we pre-specify theta thresholds beforehand. We use empirical evidence to -
+    ## -- determine the thresholds. Use CDF of logistic function for this. Done within the function.
+
+    ### -- FUNCTION ARGUMENTS
+  ## -- formula: refers to the usual formula that a user provides in any regression modeling. 
+  ## -- df: dataframe that the user wants the method/function applied to.
+  ## -- method: specify if user wants to use logistic regression (method == "lr") or proportional odds logistic regression (method == "polr")
+
+
+    ### -- OUTPUT
+  ## -- beta: this is computed for both lr and polr. For polr, we don't obtain an intercept as this is captured in theta.
+          # - beta refers to the fixed effects of the covariates
+  ## -- theta: this is computed only for polr. 
+          # - the theta values can be used to compare probabilities between different groups.
+
+
+# Estimating lr and polr --------------------------------------------------
+
+glm_v2 <- function(formula, df, method){
+
+  ## extract response variable name
+  response = as.character(formula)[2]
+
+  ## extract the response variable
+  y = df %>% dplyr::select(all_of(response))
+  
+  if(method == "lr" & nrow(unique(y)) != 2){
+    stop("Make sure that the response variable has 2 categories.")
+  }
+  
+  ## convert this response to a matrix
+  y_mat = as.matrix(y)
+  y_mat_num = apply(y_mat, 2, as.numeric)
+  
+  ## extract covariate names
+  covariates = as.character(formula)[3]
+  covariates = strsplit(covariates, "\\s*\\+\\s*")[[1]]
+  
+  ## convert the covariates to a matrix
+  x_mat = as.matrix(df %>% dplyr::select(all_of(covariates)), nrow = nrow(df))
+  n_covariates = ncol(x_mat)
+  
+  if(method == "polr"){
+    ## extract the theta names (based on the categories in the data)
+    vec = sort(pull(unique(y)))
+    theta_names = paste0("theta", vec[-length(vec)], vec[-1])
+    
+
+    ## obtain theta thresholds (from data) - these are what we would be using as the initial parameter estimates to the model
+    ## -- however, the user has autonomy to pick their own estimates. If none given, then this is what will be used as default.
+    cum_probs = cumsum(prop.table(table(y_mat)))[1:4]
+    theta_init = qlogis(cum_probs)  
+    
+    ## the default initial parameter estimates for the beta (fixed effect) coefficients are all zero (no intercept in prop odds)
+    ## -- however, again as before, the user has autonomy to pick their own initialization. 
+    
+    beta_init = rep(0, n_covariates) 
+    
+    ## -- using the above, we optimize our likelihood using the BFGS algorithm
+    
+    ## -- first get our initial estimates
+    
+    params_init = c(beta_init, theta_init)
+    names(params_init) = c(covariates, theta_names)
+    
+    ## -- now for the algorithm, we directly use BFGS
+    
+    fit = optimx::optimx(
+      par = params_init,
+      fn = log_lik_prop_odds,
+      gr = score_prop_odds,
+      method = "BFGS",
+      control = 
+        list(
+          trace = 0,
+          maximize = T,
+          abstol = T
+        ),
+      x = x_mat, 
+      y = y_mat_num,
+      n_covs = n_covariates
+    )
+    
+    rownames(fit) = NULL
+    
+    
+    return(list(beta = unlist(fit[covariates]), 
+                theta = unlist(fit[theta_names]),
+                ll = unlist(fit["value"]),
+                time = unlist(fit["xtime"])))
+    
+  }else if(method == "lr"){
+    
+    ## -- because logistic regression requires an intercept we need to add in an intercept column
+    
+    x_mat = cbind(1, x_mat)
+  
+    ## the default initial parameter estimates for the beta (fixed effect) coefficients are all zero (including the intercept)
+    ## -- however, again as before, the user has autonomy to pick their own initialization. 
+    
+    beta_init = rep(0, n_covariates + 1) 
+    names(beta_init) = c("Intercept", covariates)
+    
+    ## -- using the above, we optimize our likelihood using the NR algorithm that has been handcoded and laid out in optim.R
+    
+    fit = optim_nr_logit(beta_init = beta_init, x = x_mat, y = y_mat_num)
+    
+    estimates = as.vector(fit$estimates)
+    names(estimates) = names(beta_init)
+    
+    ll = fit$ll[length(fit$ll)]
+    time = fit$time
+    
+    return(list(beta = estimates, 
+                ll = ll,
+                time = time)
+    )
+  }
+}
+
+
+
+# Testing on heart dataset ------------------------------------------------
+
+
+    ## -- load dataset & create 2 separate response variables (1 for binary & 1 as is)
+
+df_heart <- read_csv("data/derived/df_v1.csv")
+
+    ## -- remove missing values
+df_heart <- na.omit(df_heart)
+
+    ## -- creating two response variables
+df_heart %<>%
+  dplyr::select(-...1) %>%
+  mutate(y_mult = factor(num),
+         y_bin = factor(ifelse(num == "0", 0, 1))
+  )
+
+
+    ### -- testing it for proportional odds logistic regression
+formula = y_mult ~ age + fbs + sex + cp + chol + exang + thalach
+
+      ## -- using glm_v2
+res_glm_v2 = glm_v2(formula, df = df_heart, method = "polr")
+
+      ## -- using polr function from MASS
+res_polr = polr(formula, data = df_heart)
+
+      ## -- are the coefficients the same?
+all((res_glm_v2$beta - res_polr$coefficients) < 1e-4)
+
+      ## -- is the final log-likelihood the same?
+unname(res_glm_v2$ll - logLik(res_polr) < 1e-4)
+
+
+
+
+    ### -- testing it for logistic regression
+formula = y_bin ~ age + fbs + sex + cp + chol + exang + thalach
+
+      ## -- using glm_v2
+res_glm_v2 = glm_v2(formula, df = df_heart, method = "lr")
+
+      ## -- using polr function from MASS
+res_glm = glm(formula, data = df_heart, family = "binomial")
+
+      ## -- are the coefficients the same?
+all((res_glm_v2$beta - res_glm$coefficients) < 1e-4)
+
+      ## -- is the final log-likelihood the same?
+unname(res_glm_v2$ll - logLik(res_glm) < 1e-4)
+
+
+
+
+
+
---
+++ code/optim_v2.R
@@ -0,0 +1,219 @@
+#### ---- this script serves the purpose of building the NR algorithm from scratch to solve for the parameters in the -
+### logistic regression model
+
+#### ---- NR has not been done for the proportional odds model due to time constraints. BFGS directly from optim was used for this.
+
+#### ---- to implement BFGS we still require the objective function (likelihood), first and second derivative functions
+#### ---- these have been implemented in this script, and will be used in the estimation.R script to perform estimation
+
+# Load libraries ----------------------------------------------------------
+
+library(tidyverse)
+library(ggplot2)
+library(magrittr)
+library(glue)
+library(glmnet)
+library(numDeriv)
+library(MASS)
+
+
+# Log-likelihood Functions -------------------------------------------------
+
+  ## -- ll for logistic regression (lr)
+log_lik_logistic <- function(beta, x, y){
+  
+  n = nrow(x) # number of observations
+  p = ncol(x) # number of covariates
+  eta = x %*% beta  # linear predictor
+  loglik = 0
+  
+  for(i in 1:n){
+    y_i = as.numeric(y[i,1])
+    eta_i = eta[i]
+    p_i = plogis(eta_i)
+    # avoid log(0) by adding small constant
+    loglik = loglik + y_i * log(p_i + 1e-10) + (1 - y_i) * log(1 - p_i + 1e-10)
+  }
+  return(loglik)
+}
+
+  ## -- ll for proportional odds lr
+log_lik_prop_odds <- function(params, x, y, n_covs) {
+  
+  beta = params[1:n_covs]
+  theta = params[n_covs+1:length(params)]
+  
+  n = nrow(x)        # number of observations
+  p = ncol(x)        # number of predictors
+  J = length(unique(y))         # number of outcome categories (assumes y is a vector of 1:J)
+  eta = x %*% beta   # linear predictor: eta_i = x_i^T * beta
+  
+  # Step 1: Compute cumulative probabilities gamma[,j] = P(Y <= j)
+  gamma = matrix(0, nrow = n, ncol = J)
+  for (j in 1:(J - 1)) {
+    gamma[, j] = plogis(theta[j] - eta)
+  }
+  gamma[, J] = 1  # By definition, P(Y <= J) = 1
+  
+  # Step 2: Compute category probabilities: P(Y = j) = gamma_j - gamma_{j-1}
+  probs = matrix(0, nrow = n, ncol = J)
+  for (j in 1:J) {
+    lower = if (j == 1) rep(0, n) else gamma[, j - 1]
+    probs[, j] = gamma[, j] - lower
+  }
+  
+  # Step 3: Avoid log(0) by clipping probabilities
+  eps = 1e-12
+  probs = pmax(probs, eps)
+  
+  # Step 4: Compute log-likelihood: sum over log P(y_i | x_i)
+  # This pulls the probability for each observation’s actual category
+  ll = sum(log(probs[cbind(1:n, y + 1)]))
+  
+  # Return log-likelihood and intermediate structures
+  return(ll)
+}
+
+
+
+# Score Function (gradient) -----------------------------------------------
+
+
+  ## -- score for lr
+score_logit <- function(beta, x, y){
+  
+  xb = x %*% beta
+  pi = exp(xb) / (1 + exp(xb))
+  y = as.matrix(y, nrow = nrow(y))
+  
+  score = matrix(data = unname(t(x) %*% (y - pi)), nrow = length(beta), ncol = 1)
+  
+  return(score)
+}
+
+  ## -- score for proportional odds lr
+score_prop_odds <- function(params, x, y, n_covs) {
+  
+  beta = params[1:n_covs]
+  theta = params[(n_covs+1):length(params)]
+  
+  n = nrow(x)               # Number of observations
+  p = length(beta)          # Number of predictors
+  J = length(theta) + 1     # Number of outcome categories
+  
+  eta = x %*% beta          # Linear predictor (n x 1)
+  
+  grad_beta = rep(0, p)     # Gradient vector for beta
+  grad_theta = rep(0, J - 1) # Gradient vector for theta
+  
+  eps = 1e-12               # Small number for numerical stability
+  
+  for (i in 1:n) {
+    x_i = x[i, ]
+    eta_i = eta[i]
+    for (j in 1:J) {
+      # Threshold transformations
+      t_j    = if (j <= J - 1) theta[j] - eta_i else Inf
+      t_jm1  = if (j == 1) -Inf else theta[j - 1] - eta_i
+      # CDF and PDF values
+      F_j    = plogis(t_j)
+      F_jm1  = plogis(t_jm1)
+      f_j    = dlogis(t_j)
+      f_jm1  = if (j == 1) 0 else dlogis(t_jm1)
+      # Probability of being in category j
+      pi_ij  = F_j - F_jm1
+      pi_ij  = max(pi_ij, eps)  # Avoid divide-by-zero or log(0)
+      indicator = as.numeric(y[i] == j - 1)  # 1 if cat is j
+      # Gradient wrt beta
+      grad_beta = grad_beta + indicator * ((f_jm1 - f_j) / pi_ij) * x_i
+      # Gradient wrt theta
+      if (j <= J - 1) {
+        grad_theta[j] = grad_theta[j] + indicator * (f_j / pi_ij)
+      }
+      if (j > 1) {
+        grad_theta[j - 1] = grad_theta[j - 1] - indicator * (f_jm1 / pi_ij)
+      }
+    }
+  }
+  
+  return(c(grad_beta, grad_theta))
+}
+
+
+# Hessian matrices (2nd derivative) ---------------------------------------
+
+  ## -- note that not hessian has been derived for the proportional odds lr because it is not required to implement BFGS
+
+  ## -- hessian for lr
+hessian_logit <- function(beta_t, x, y){
+  
+  xb = x %*% beta_t
+  pi = exp(xb) / (1 + exp(xb))
+  
+  W = diag(c(1-pi)*c(pi), nrow =n)
+  
+  hessian = matrix(data = t(x) %*% W %*% x, nrow = length(beta_t), ncol = length(beta_t))
+  
+  return(hessian)
+}
+
+
+
+# Helper functions --------------------------------------------------------
+
+  ## -- the h(t) function in NR is simply = inverse(hessian) * score. This is done in the function below for logistic regression.
+
+h_t_lr <- function(beta_t, x, y){
+  
+  n_params = length(beta_t)
+  score = as.matrix(unlist(score_logit(beta = beta_t, x, y)), nrow = n_params)
+  
+  hessian = hessian_logit(beta_t, x, y)
+  
+  return(inverse(hessian) %*% 
+           score)
+}
+
+
+
+# Newton Raphson (LR) -----------------------------------------------------
+
+  ## -- the function below can be used to optimize the parameters in a logistic regression model
+
+optim_nr_logit <- function(beta_init, x, y, tol = 1e-3, iter = 1e3){
+  
+  start_time = Sys.time()
+  
+  log_lik_vector = rep(NA, iter)
+  
+  for(i in 1:iter){
+    #print(glue("Iteration: {i}"))
+    if(i == 1){
+      
+      beta_t = as.vector(beta_init + h_t_lr(beta_init, x, y))
+      
+      log_lik_vector[i] = log_lik_logistic(beta_t, x, y)
+      
+    }else{
+      
+      ll_prev = log_lik_vector[i-1]
+      
+      beta_t = beta_t + h_t_lr(beta_t, x, y)
+      
+      ll_t = log_lik_logistic(beta_t, x, y)
+      
+      log_lik_vector[i] = ll_t
+      
+      if(abs(ll_prev - ll_t) < tol){
+        end_time = Sys.time()
+        print(glue("Algorithm converged in {i} iterations."))
+        #names(beta_t) = colnames(x); names(beta_t)[1] = "Intercept"
+        return(list(estimates = beta_t, 
+                    ll = log_lik_vector[!is.na(log_lik_vector)], 
+                    time = end_time - start_time))
+      }
+    }
+  }
+  print("Algorithm did not converge")
+  return(list(estimates = NA, ll = log_lik_vector))
+}
